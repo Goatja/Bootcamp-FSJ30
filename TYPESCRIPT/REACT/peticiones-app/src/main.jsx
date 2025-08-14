74f2925eaa9f1db3def19883203d3b05c6bdf266
Goatja <jorge.arana528@gmail.com>
2025-08-13 20:30:51 -0600

    new file about promises

---
+++ TYPESCRIPT/REACT/peticiones-app/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+
+//SWC -> Speedy Web Compiler => Compilador que traduce el codigo en la web de forma rapida
